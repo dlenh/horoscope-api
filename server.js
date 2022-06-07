864ddf0f124a954474d2cc1117c7bab6bc089143
David Lenh <dlenh425@gmail.com>
2022-06-07 15:04:32 -0700

    Added properties to zodiac API

--- server.js
+++ server.js
@@ -7,78 +7,90 @@ app.use(cors());
 
 arr = ["rooster", "dog", "pig", "rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat", "monkey"];
 
-const horoscope = {
+const zodiac = {
     "rat": {
         "compatible": ["dragon", "monkey", "rat"],
         "incompatible": ["horse", "goat"],
         "element": "water",
-        "traits":
+        "good traits": ["charming", "intelligent", "easy to get along with"],
+        "bad traits": ["overambitious at times", "easily agitated"]
     },
     "ox": {
         "compatible": ["snake", "rooster", "ox"],
         "incompatible": ["goat", "horse"],
         "element": "earth",
-        "traits":
+        "good traits": ["calm", "dependable", "patient"],
+        "bad traits": ["stubborn", "naive"]
     },
     "tiger": {
         "compatible": ["horse", "dog", "tiger"],
         "incompatible": ["monkey", "snake"],
         "element": "wood",
-        "traits":
+        "good traits": ["passionate", "daring", "adventurous"],
+        "bad traits": ["impulsive", "reckless", "rebellious"]
     },
     "rabbit": {
         "compatible": ["pig", "goat", "rabbit"],
         "incompatible": ["rooster", "dragon"],
         "element": "wood",
-        "traits":
+        "good traits": ["artistic", "strong-willed", "graceful"],
+        "bad traits": ["soft-spoken", "ruthless"]
     },
     "dragon": {
         "compatible": ["rat", "monkey", "dragon"],
         "incompatible": ["dog", "rabbit"],
         "element": "earth",
-        "traits":
+        "good traits": ["energetic", "loyal", "whimsical"],
+        "bad traits": ["prone to burnout", "egoistical"]
     },
     "snake": {
         "compatible": ["ox", "rooster", "snake"],
         "incompatible": ["pig", "tiger"],
         "element": "fire",
-        "traits":
+        "good traits": ["tenacious", "wise"],
+        "bad traits": ["possessive", "demanding"]
     },
     "horse": {
         "compatible": ["dog", "tiger", "horse"],
         "incompatible": ["rat", "ox"],
         "element": "fire",
-        "traits":
+        "good traits": ["cheerful", "quick-witted"],
+        "bad traits": ["hot-tempered", "impulsive"]
     },
     "goat": {
         "compatible": ["rabbit", "pig", "goat"],
         "incompatible": ["ox", "rat"],
         "element": "earth",
-        "traits":
+        "good traits": ["righteous", "creative", "generous"],
+        "bad traits": ["withdrawn", "indecisive", "pessimistic"]
     },
     "monkey": {
         "compatible": ["dragon", "rat", "monkey"],
         "incompatible": ["tiger", "pig"],
         "element": "metal",
-        "traits":
+        "good traits": ["innovative", "quick-witted", "sociable"],
+        "bad traits": ["egocentric", "docile"]
     },
     "rooster": {
         "compatible": ["ox", "snake", "rooster"],
         "incompatible": ["rabbit", "dog"],
         "element": "metal",
-        "traits":
+        "good traits": ["decisive", "meticulous"],
+        "bad traits": ["talkative", "aggressive"]
     },
     "dog": {
         "compatible": ["tiger", "horse", "dog"],
         "incompatible": ["dragon", "rooster"],
         "element": "earth",
-        "traits":
+        "good traits": ["honest", "straightfoward", "loyal"],
+        "bad traits": ["selfless", "outspoken"]
     },
     "pig": {
         "compatible": ["rabbit", "goat", "pig"],
         "incompatible": ["snake", "monkey"],
         "element": "water",
-        "traits":
+        "good traits": ["courageous", "thoughtful", "determined"],
+        "bad traits": ["gullible", "naive"]
     }
 }
 
