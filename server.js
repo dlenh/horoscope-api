1af90f27bb2ef8fba662defff76d773bb33b9e94
David Lenh <dlenh425@gmail.com>
2022-06-07 11:34:21 -0700

    Compiled info for zodiac api

--- server.js
+++ server.js
@@ -8,18 +8,78 @@ app.use(cors());
 arr = ["rooster", "dog", "pig", "rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat", "monkey"];
 
 const horoscope = {
-    "rat": {},
-    "ox": {},
-    "tiger": {},
-    "rabbit": {},
-    "dragon": {},
-    "snake": {},
-    "horse": {},
-    "goat": {},
-    "monkey": {},
-    "rooster": {},
-    "dog": {},
-    "pig": {}
+    "rat": {
+        "compatible": ["dragon", "monkey", "rat"],
+        "incompatible": ["horse", "goat"],
+        "element": "water",
+        "traits":
+    },
+    "ox": {
+        "compatible": ["snake", "rooster", "ox"],
+        "incompatible": ["goat", "horse"],
+        "element": "earth",
+        "traits":
+    },
+    "tiger": {
+        "compatible": ["horse", "dog", "tiger"],
+        "incompatible": ["monkey", "snake"],
+        "element": "wood",
+        "traits":
+    },
+    "rabbit": {
+        "compatible": ["pig", "goat", "rabbit"],
+        "incompatible": ["rooster", "dragon"],
+        "element": "wood",
+        "traits":
+    },
+    "dragon": {
+        "compatible": ["rat", "monkey", "dragon"],
+        "incompatible": ["dog", "rabbit"],
+        "element": "earth",
+        "traits":
+    },
+    "snake": {
+        "compatible": ["ox", "rooster", "snake"],
+        "incompatible": ["pig", "tiger"],
+        "element": "fire",
+        "traits":
+    },
+    "horse": {
+        "compatible": ["dog", "tiger", "horse"],
+        "incompatible": ["rat", "ox"],
+        "element": "fire",
+        "traits":
+    },
+    "goat": {
+        "compatible": ["rabbit", "pig", "goat"],
+        "incompatible": ["ox", "rat"],
+        "element": "earth",
+        "traits":
+    },
+    "monkey": {
+        "compatible": ["dragon", "rat", "monkey"],
+        "incompatible": ["tiger", "pig"],
+        "element": "metal",
+        "traits":
+    },
+    "rooster": {
+        "compatible": ["ox", "snake", "rooster"],
+        "incompatible": ["rabbit", "dog"],
+        "element": "metal",
+        "traits":
+    },
+    "dog": {
+        "compatible": ["tiger", "horse", "dog"],
+        "incompatible": ["dragon", "rooster"],
+        "element": "earth",
+        "traits":
+    },
+    "pig": {
+        "compatible": ["rabbit", "goat", "pig"],
+        "incompatible": ["snake", "monkey"],
+        "element": "water",
+        "traits":
+    }
 }
 
 
