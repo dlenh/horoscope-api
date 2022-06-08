acc4d59c1b556fd21ba1d45a019d65133a316cf8
David Lenh <dlenh425@gmail.com>
2022-06-08 11:02:25 -0700

    Commented out async/await

--- main.js
+++ main.js
@@ -3,7 +3,18 @@ document.querySelector("button").addEventListener("click", apiRequest);
 arr = ["Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat"];
 
 function apiRequest() {
-    const year = document.querySelector("input").value
-    document.querySelector("h2").innerText = arr[year%12];
+    const year = document.querySelector("input").value;
+    const zodiac = document.querySelector("#zodiac");
+    // try {
+    //     const res = await fetch() 
+    //     const data = await res.json()
+
+    //     console.log(data)
+    // }
+    if (year === "") {
+        zodiac.innerText = "";
+    } else {
+       zodiac.innerText = arr[year%12];
+    }
     return false;
 }
